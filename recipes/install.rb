4727c5073e8b800b5186879851e0beb0e6c3fb47
pyama86 <pyama@pepabo.com>
2019-12-24 10:34:02 +0900

    fix pth

--- recipes/install.rb
+++ recipes/install.rb
@@ -1,12 +1,12 @@
 c = Chef::Config[:file_cache_path]
 name = "oauth2_proxy-v#{node["oauth2_proxy"]['version']}.linux-amd64.go#{node["oauth2_proxy"]['goversion']}.tar.gz"
-remote_file "#{c}/#{name}.tar.gz" do
+remote_file "#{c}/#{name}" do
   source "https://github.com/pusher/oauth2_proxy/releases/download/v#{node["oauth2_proxy"]['version']}/#{name}"
   notifies :run, 'execute[install_oauth2_proxy]'
 end
 
 execute 'install_oauth2_proxy' do
-  command "tar xf #{name}.tar.gz;mv release/oauth2_proxy-linux-amd64 /usr/bin/oauth2_proxy"
+  command "tar xf #{name};mv release/oauth2_proxy-linux-amd64 /usr/bin/oauth2_proxy"
   cwd c
   action :nothing
 end
