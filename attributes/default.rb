75da87b908454c8e74387e4dbebc298b2a45a5b1
pyama86 <pyama@pepabo.com>
2020-05-27 16:36:46 +0900

    use latest

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
 
-default["oauth2_proxy"]['version'] = '4.1.0'
-default["oauth2_proxy"]['goversion'] = '1.13.5'
+default["oauth2_proxy"]['version'] = '5.1.1'
+default["oauth2_proxy"]['goversion'] = '1.14.2'
